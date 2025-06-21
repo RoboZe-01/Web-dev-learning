11d773b69c700e12d9fc260351c65ce6c6d388bf
RoboZe <roboticsxzero@gmail.com>
2025-06-22 00:23:11 +0530

    Installed dependencies and made a React file

--- React From basics to advance/Basics/basics.js
+++ React From basics to advance/Basics/basics.js
@@ -1,7 +1,17 @@
-var h1 = document.createElement('h1')
-var root =document.querySelector('#root')
+// var h1 = document.createElement('h1')
+// var root =document.querySelector('#root')
 
 
 
-h1.innerHTML="Using Javascript"
-root.appendChild(h1)
+// h1.innerHTML="Using Javascript"
+// root.appendChild(h1)
+
+
+
+
+var h1 = React.createElement('h1',null,"Hello from React")
+var parent = document.querySelector("#parent")
+// console.log(parent)
+var root = ReactDOM.createRoot(parent)
+root.render(h1)
+
