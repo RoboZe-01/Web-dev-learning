b3c4a4394b59b23e0cefcdaaf0a76b60ad3314e0
RoboZe <roboticsxzero@gmail.com>
2025-06-25 10:12:31 +0530

    html css done now js starte for the random password project

---
+++ mini Projects'/Random PassWord Generator/Main Code/script.js
@@ -0,0 +1,16 @@
+const passwordBox =document.getElementById("password");
+const lenght = 12;
+
+
+
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const lowerCase = "abcdefghijklmnopqrstuvwxyz"
+
+const numbers ="0123456789"
+const symbols = "~!@#$%&*_+-()+|}{[]></+"
+
+function createPassword(){
+
+    
+}
