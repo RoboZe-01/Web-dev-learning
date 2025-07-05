55ef4d64d83e9c6960d628fa5bdb2c273e727155
RoboZe <roboticsxzero@gmail.com>
2025-07-05 16:35:02 +0530

    Github checking

--- React From basic to advance/src/App.jsx
+++ React From basic to advance/src/App.jsx
@@ -1,16 +1,2 @@
 
-import React from 'react'
 
-const App = () => {
-  let name = "Prem";
-  const changeName = () => {
-    name = "Rahul";
-
-  return (
-    <div>
-     <h1>UserName is {name}</h1>
-    </div>
-  )
-}
-}
-export default App
