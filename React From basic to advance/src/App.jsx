ae16e6823a47440ff2ead7f71dfb7c9b7c5ea99c
RoboZe <roboticsxzero@gmail.com>
2025-07-05 16:54:35 +0530

    Hooks done from the video

--- React From basic to advance/src/App.jsx
+++ React From basic to advance/src/App.jsx
@@ -1,2 +1,20 @@
 
+import React from 'react'
 
+const App = () => {
+  let name = "Prem";
+
+    
+  const changeName = () => {
+const [first, setfirst] = useState(second)
+  }
+
+  return (
+    <div>
+     <h1>UserName is {name}</h1>
+     <button onClick={changeName}>Change USer</button>
+    </div>
+  )
+}
+
+export default App
