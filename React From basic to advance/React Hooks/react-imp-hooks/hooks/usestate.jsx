0dd7d87436d583e41bf01c0d19ee8669be424dd2
RoboZe <roboticsxzero@gmail.com>
2025-07-15 22:51:10 +0530

    Started usestate hook

---
+++ React From basic to advance/React Hooks/react-imp-hooks/hooks/usestate.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useState } from 'react'
+
+const usestate = () => {
+    const [first, setfirst] = useState(second)
+  return (
+    <div>
+      <h1>Hiihow are you first</h1>
+    </div>
+  )
+}
+
+export default usestate
